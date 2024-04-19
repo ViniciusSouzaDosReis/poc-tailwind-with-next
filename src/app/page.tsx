02a871faccf59406d462135b8e073f7c6b721654
Vinicius Reis <vinireis010305@gmail.com>
2024-04-19 12:04:05 -0300

    structure layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-slate-800 p-4">
-      <h1 className="text-5xl font-bold text-fuchsia-400">Hello World</h1>
-    </div>
-  )
+  return <h1 className="text-1xl font-bold">Hello World</h1>
 }
