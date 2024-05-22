acf782cc4725ac07e88b8adce599426d9d4d98d9
Vinicius Reis <vinireis010305@gmail.com>
2024-05-22 16:10:03 -0300

    refactor: remove duplicate CSS class in FileInput Trigger component

--- src/components/Form/FileInput/Trigger.tsx
+++ src/components/Form/FileInput/Trigger.tsx
@@ -9,7 +9,7 @@ export function Trigger() {
   return (
     <label
       htmlFor={id}
-      className="group flex w-full cursor-pointer cursor-pointer flex-col items-center justify-center gap-3 rounded-lg border border-zinc-200 px-6 py-4 shadow-sm hover:border-violet-300"
+      className="group flex w-full cursor-pointer flex-col items-center justify-center gap-3 rounded-lg border border-zinc-200 px-6 py-4 shadow-sm hover:border-violet-300"
     >
       <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-25 group-hover:bg-violet-100">
         <CloudUpload className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
