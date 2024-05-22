d40121cd2ee72674bed9596e5d6f8b55c9bed8f4
Vinicius Reis <vinireis010305@gmail.com>
2024-05-22 19:19:48 -0300

    feat: Add useAutoAnimate hook to FileList component

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -3,12 +3,14 @@
 import { File, Trash2 } from 'lucide-react'
 import { useFileInput } from './Root'
 import { formatFileSize } from '@/utils/format-bytes'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export function FileList() {
   const { files } = useFileInput()
+  const [parent] = useAutoAnimate()
 
   return (
-    <div className="mt-4 space-y-3">
+    <div className="mt-4 space-y-3" ref={parent}>
       {files.map((file) => (
         <div
           key={file.name}
